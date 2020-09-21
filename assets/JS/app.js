37033501057b9e5747c36314d6fca322b4039411
Ishtiak Ahmed Evan <ishtiak.ahmed.evan24@gmail.com>
2020-09-22 04:36:18 +0600

    done a lot

---
+++ assets/JS/app.js
@@ -0,0 +1,22 @@
+const showMenu = (toggleID,navID) => {
+    const toggle = document.getElementById(toggleID),
+    nav = document.getElementById(navID)
+
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+
+showMenu('nav-toggle','nav-menu')
+
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
